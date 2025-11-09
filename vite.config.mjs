fd468dd4feb15a38d91f13e7e6151b8528e8971c
Krishna Kumar Amatya <mandalkrish47@gmail.com>
2025-11-10 04:15:14 +0530

    cleanup local build artifacts

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 5173,
-  },
-  base: "./", // important for relative asset paths on Vercel
+  base: "./",
+  build: { outDir: "dist" },
+  server: { port: 5173 },
 });
